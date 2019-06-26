7dc34196f6d95f2b71f7e0e49a665b3c9c28c2e5
jluoxin <13834144912@163.com>
2019-06-26 14:13:52 +0800

    增加paramiko包

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'ccodt',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
-        'HOST': '192.168.33.10',                   # 开发的时候，使用localhost
+        'PASSWORD': 'root',                        # 你的数据库password
+        'HOST': '10.130.44.108',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
